41d5a76bd45c90b3c4c02e164a5330cef342f98d
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-20 18:53:50 +0200

    Update category.validator.ts

--- src/facts/validators/category.validator.ts
+++ src/facts/validators/category.validator.ts
@@ -6,11 +6,15 @@ import { CategoryEnum } from '../category.enum';
 
 @ValidatorConstraint({ name: 'IsCategory', async: false })
 export class IsCategory implements ValidatorConstraintInterface {
-  validate(text: string) {
-    return (Object.values(CategoryEnum) as string[]).includes(text);
+  private readonly allowedCategories = Object.values(CategoryEnum).filter(
+    (category: string) => category !== 'all',
+  );
+
+  validate(text: CategoryEnum) {
+    return this.allowedCategories.includes(text);
   }
 
   defaultMessage() {
-    return `Category be one of the following values: ${Object.values(CategoryEnum).join(', ')}`;
+    return `Category be one of the following values: ${this.allowedCategories.join(', ')}`;
   }
 }
