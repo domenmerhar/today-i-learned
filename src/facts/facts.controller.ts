19796fe3472a6239289c6ab22d9b50d8c15b197b
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-20 18:55:22 +0200

    Update facts.controller.ts

--- src/facts/facts.controller.ts
+++ src/facts/facts.controller.ts
@@ -6,11 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { FactsService } from './facts.service';
 import { CreateFactDto } from './dto/create-fact.dto';
 import { Fact } from './fact.entity';
-import { CategoryDto } from './dto/category.dto';
+import { VoteEnum } from './vote.enum';
+import { VoteValidationPipe } from './pipes/vote.validator';
 
 @Controller('facts')
 export class FactsController {
@@ -27,17 +29,20 @@ export class FactsController {
   }
 
   @Patch(':id/add-vote')
-  addVote(@Param('id') id: string, @Body() categoryDto: CategoryDto) {
-    return this.factsService.addVote(id, categoryDto);
+  addVote(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body('vote', VoteValidationPipe) vote: VoteEnum,
+  ) {
+    return this.factsService.addVote(id, vote);
   }
 
   @Patch(':id/remove-vote')
-  removeVote(@Param('id') id: string, @Body() categoryDto: CategoryDto) {
-    return this.factsService.removeVote(id, categoryDto);
+  removeVote(@Param('id') id: string, @Body('vote', VoteValidationPipe) vote) {
+    return this.factsService.removeVote(id, vote);
   }
 
   @Delete(':id')
-  removeFact(@Param('id') id: string): Promise<string> {
+  removeFact(@Param('id', ParseUUIDPipe) id: string): Promise<string> {
     return this.factsService.removeFact(id);
   }
 }
