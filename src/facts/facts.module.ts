95b78c91553aa6abb4a7b95b3aecae67abd27b66
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-20 15:33:31 +0200

    Update facts.module.ts

--- src/facts/facts.module.ts
+++ src/facts/facts.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { FactsService } from './facts.service';
 import { FactsController } from './facts.controller';
 import { FactsRepository } from './facts.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Fact } from './fact.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Fact])],
   controllers: [FactsController],
   providers: [FactsService, FactsRepository],
 })
