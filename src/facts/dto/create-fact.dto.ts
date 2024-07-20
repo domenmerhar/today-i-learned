91e809f0aee1a65746c3b89eb579ac307acebd1f
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-20 18:40:59 +0200

    Updadte create-fact.dto.ts

--- src/facts/dto/create-fact.dto.ts
+++ src/facts/dto/create-fact.dto.ts
@@ -1,9 +1,19 @@
+import { IsNotEmpty, Matches, MaxLength, Validate } from 'class-validator';
 import { CategoryType } from '../category.type';
+import { IsCategory } from '../validators/category.validator';
 
 export class CreateFactDto {
+  @IsNotEmpty()
+  @MaxLength(255)
   description: string;
 
+  @Matches(
+    "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)'",
+    '',
+    { message: 'Invalid URL' },
+  )
   source: string;
 
+  @Validate(IsCategory)
   category: CategoryType;
 }
