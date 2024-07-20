8167888f567cc253429bb6abf4b37c0899fcb9ba
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-20 11:58:51 +0200

    Add category.enum.ts

---
+++ src/facts/category.enum.ts
@@ -0,0 +1,11 @@
+export enum CategoryEnum {
+  ALL = 'all',
+  TECHNOLOGY = 'technology',
+  SCIENCE = 'science',
+  FINANCE = 'finance',
+  SOCIETY = 'society',
+  ENTERTAINMENT = 'entertainment',
+  HEALTH = 'health',
+  HISTORY = 'history',
+  NEWS = 'news',
+}
