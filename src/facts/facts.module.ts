db8aac0ed972cf5ced69f118be5fdac94aa374b8
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-20 18:54:28 +0200

    Update facts.module.ts

--- src/facts/facts.module.ts
+++ src/facts/facts.module.ts
@@ -4,10 +4,12 @@ import { FactsController } from './facts.controller';
 import { FactsRepository } from './facts.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Fact } from './fact.entity';
+import { IsCategory } from './validators/category.validator';
+import { VoteValidationPipe } from './pipes/vote.validator';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Fact])],
   controllers: [FactsController],
-  providers: [FactsService, FactsRepository],
+  providers: [FactsService, FactsRepository, IsCategory, VoteValidationPipe],
 })
 export class FactsModule {}
