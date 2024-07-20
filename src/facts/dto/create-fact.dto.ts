d797a7aa9b59f0fd41714627132dfb347cc393f6
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-20 18:58:53 +0200

    Update URL validation regex in create-fact.dto.ts

--- src/facts/dto/create-fact.dto.ts
+++ src/facts/dto/create-fact.dto.ts
@@ -8,7 +8,7 @@ export class CreateFactDto {
   description: string;
 
   @Matches(
-    "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)'",
+    'https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&\\/\\/=]*)',
     '',
     { message: 'Invalid URL' },
   )
