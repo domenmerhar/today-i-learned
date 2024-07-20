017defdd11e1d0f75d9c759d81e9d5c791244dfc
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-20 15:33:13 +0200

    Upate create-fact.dto.ts

--- src/facts/dto/create-fact.dto.ts
+++ src/facts/dto/create-fact.dto.ts
@@ -1,1 +1,19 @@
-export class CreateFactDto {}
+import { CategoryEnum } from '../category.enum';
+
+export class CreateFactDto {
+  description: string;
+
+  source: string;
+
+  category: CategoryType;
+}
+
+type CategoryType =
+  | CategoryEnum.TECHNOLOGY
+  | CategoryEnum.SCIENCE
+  | CategoryEnum.FINANCE
+  | CategoryEnum.SOCIETY
+  | CategoryEnum.ENTERTAINMENT
+  | CategoryEnum.HEALTH
+  | CategoryEnum.HISTORY
+  | CategoryEnum.NEWS;
