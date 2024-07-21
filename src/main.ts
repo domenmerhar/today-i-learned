563b9ed3cf122834c000a7ca745cdc8a51262d29
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-21 09:33:22 +0200

    Update main.ts; enable CORS for API requests

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   await app.listen(3000);
 }
