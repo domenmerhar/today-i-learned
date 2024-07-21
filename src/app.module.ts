f5b1def2146b7006e92b928ef197f34ae693b2e1
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-21 10:10:44 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FactsModule } from './facts/facts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
     FactsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'facts',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: process.env.STAGE === 'dev',
+      }),
     }),
   ],
   controllers: [AppController],
